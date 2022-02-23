8303d6dcb599390aad0f7b6b713393b27f9128f8
SalmanulFarisKA <98825604+SalmanulFarisKA@users.noreply.github.com>
2022-02-24 04:09:10 +0530

    Update Password Bank - GUI (Unfinished).py

--- Password Bank - GUI (Unfinished).py
+++ Password Bank - GUI (Unfinished).py
@@ -15,6 +15,8 @@ INSERT INTO sPasswords VALUES("google", "1234")
 
 import mysql.connector
 from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import *
 import random
 
 # Connecting to database
@@ -32,7 +34,9 @@ def raise_frame(frame):  # to change frames
 def close(window):  # to close the window
     window.destroy()
 
-# Windows #
+
+def placeholder():
+    print("It works")
 
 
 # root window
@@ -40,21 +44,6 @@ root = Tk()
 root.geometry("350x200")  # size of the window width: 350, height: 200
 root.resizable(0, 0)  # this prevents from resizing the window
 root.title("Password Manager")  # title
-
-# Frames #
-
-# For root window
-fmPassLogin = Frame(root)
-fStart = Frame(root)
-fSavePass = Frame(root)
-fRetrievePass = Frame(root)
-fShowReplacedPass = Frame(root)
-fShowSavedPass = Frame(root)
-fRetrieveSinglePass = Frame(root)
-fRetrieveAllPass = Frame(root)
-
-for frame in (fmPassLogin, fStart, fSavePass, fRetrievePass, fShowSavedPass):
-    frame.grid(row=0, column=0, sticky="news")
 
 # Functions #
 
@@ -73,7 +62,7 @@ def login():
     pass_id = mpassword.get()
 
     # Query the database
-    c.execute("SELECT * FROM MasterPassword")
+    c.execute("SELECT * FROM MPassword")
     records = c.fetchall()
 
     # Loop through the results
@@ -82,7 +71,7 @@ def login():
         if record[0] == pass_id:
             mpassFound = True
             mpassword.delete(0, END)
-            raise_frame(fStart)
+            openfStart()
     if not mpassFound:
         mpassword.delete(0, END)
         mpass_error_label.config(text="Wrong password, try again.")
@@ -92,220 +81,94 @@ def login():
     conn.close()
 
 
-# Create a Function to Check if the password exists for a username
-
-def checkSPass():
-
-    # Connect to a database
-    conn = mysql.connector.connect(host="localhost", user="root", passwd="1234",
-                                   database="PasswordManager", auth_plugin='mysql_native_password')
-    # Create cursor
-    c = conn.cursor()
-
-    # Getting information from text box
-    user_id = aAccName.get()
-
-    # Query the database
-    c.execute("SELECT * FROM sPasswords")
-    records = c.fetchall()
-
-    # Loop through the results
-    userFound = False
-    for record in records:
-        if record[0] == user_id:
-            userFound = True
-            # If the username is found then ask if you want to replace the password
-            openReplacePass()
-
-    if not userFound:
-        # PLACEHOLDER: function that generates a new password and saves it to database like replacePassYes
-        raise_frame(fShowSavedPass)
-
-    conn.commit()
-
-    conn.close()
-
-# Create Function to open a replace password window
-
-
-def openReplacePass():  # The window is not opening
-
-    # Saved password replace window
-    # Toplevel object which will be treated as a new window
-    sReplace = Toplevel(root)
-    sReplace.title("Replace Password")
-    sReplace.geometry("200x150")
-    sReplace.resizable(0, 0)
-
-    # Creating Labels for Replace Saved Passwords Page
-    sPass_error_label = Label(
-        sReplace, text="That account already has a Saved password. Do you want to replace it?")
-    sPass_error_label.grid(row=0, column=0)
-    # Creating a yes button
-    sReplace_yes_btn = Button(sReplace, text="Yes",
-                              command=replacePassYes(sReplace))
-    sReplace_yes_btn.grid(row=1, column=0)
-    # Creating a no Button
-    sReplace_no_btn = Button(sReplace, text="No", command=close(sReplace))
-    sReplace_no_btn.grid(row=1, column=1)
-
-
-replacedPass = ['']
-
-
-def replacePassYes(window):
-
-    # Closing the replace window
-    close(window)
-
-    # Generate a new password
-    # Now how do you access replacedPass outside this function so you can show this in fShowReplacedPass?
-    # global replacedPass
-    replacedPass[0] = genPass()
-
-    '''replacedPass can only be used if replacePassYes is called but we need it to be there without that for fShowReplacedPass
-    I'll try declaring rP as a list (so it is mutable and then try making it global in the function)
-    '''
-
-    # Then save it to database
-
-    # Connect to a database
-    conn = mysql.connector.connect(host="localhost", user="root", passwd="1234",
-                                   database="PasswordManager", auth_plugin='mysql_native_password')
-
-    # Create cursor
-    c = conn.cursor()
-
-    # Query the database
-    # c.execute('INSERT INTO sPasswords VALUES("{}", "{}")'.format(
-    #     aAccName_text, replacedPass[0]))
-
-    c.execute('UPDATE sPasswords SET Password = "{}" WHERE Account = "{}"'.format(replacedPass[0], aAccName_text))
-
-    # Raising replacedPassword window
-    raise_frame(fShowReplacedPass)
-
-    conn.commit()
-
-    conn.close()
-
-
-def genPass():
-
-    letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    numbers = "1234567890"
-    symbols = "!@#$%^&*()"
-
-    passw = ""
-    for _ in range(10):
-        passw_letters = random.choice(letters)
-        passw += passw_letters
-    for _ in range(5):
-        passw_num = random.choice(numbers)
-        passw += passw_num
-    for _ in range(5):
-        passw_sym = random.choice(symbols)
-        passw += passw_sym
-
-    return passw
-
-
 # GUI #
 
+
 # Master Password Login Page
+fmPassLogin = Frame(root)
+fmPassLogin.pack()
 
 # Create labels for login page
 mpassword_label = Label(fmPassLogin, text="Master password")
-mpassword_label.grid(row=0, column=0)
+mpassword_label.grid(row=0, column=0, pady=50)
 mpass_error_label = Label(fmPassLogin, text="")
-mpass_error_label.grid(row=1, column=0, columnspan=2)
+mpass_error_label.grid(row=1, column=0)
 # Create text boxes for login page
 mpassword = Entry(fmPassLogin, width=30)
-mpassword.grid(row=0, column=1, padx=20)
+mpassword.grid(row=0, column=1, padx=20, pady=50, columnspan=2)
 # Create a login button
 login_btn = Button(fmPassLogin, text="Login", command=login)
-login_btn.grid(row=3, column=1)
+login_btn.grid(row=3, column=2, pady=20)
 
-# Starting Page
 
-# Create Retrieve passwords button
-retrievePass_btn = Button(
-    fStart, text="Retrieve Passwords", command=lambda: raise_frame(fRetrievePass))
-retrievePass_btn.grid(row=0, column=0)
-# Create Save a password button
-savedPass_btn = Button(fStart, text="Save a Password",
-                       command=lambda: raise_frame(fSavePass))
-savedPass_btn.grid(row=0, column=1)
+def openfStart():
 
-# Saving a password page
+    close(root)
 
-# Creating Back Button
-savedPass_back_btn = Button(fSavePass, text="Back",
-                            command=lambda: raise_frame(fStart))
-savedPass_back_btn.grid(row=0, column=0)
-# Creating Labels for Saved Passwords Page
-aAccName_label = Label(fSavePass, text="Enter Account Name: ")
-aAccName_label.grid(row=1, column=0)
-# Creating text boxes for Saved Passwords Page
-aAccName = Entry(fSavePass, width=30)
-aAccName.grid(row=1, column=1)
-aAccName_text = aAccName.get()
-# Creating Enter button
-sEnter_btn = Button(fSavePass, text="Enter", command=checkSPass)
-sEnter_btn.grid(row=2, column=1)
+    # Starting Page
 
-# Showing Replaced Pass Page
+    fStart = Tk()
+    fStart.title("Password Bank")
+    fStart.geometry("385x370")
+    fStart.resizable(0, 0)
 
-# Creating Back Button
-replacedPass_back_btn = Button(
-    fShowReplacedPass, text="Back", command=lambda: raise_frame(fSavePass))
-replacedPass_back_btn.grid(row=0, column=0)
-# Creating Labels for Replaced Pass page
-replacedPassText = Label(fShowReplacedPass, text="The saved password for {} is {}".format(
-    aAccName.get(), replacedPass[0]))
-replacedPassText.grid(row=1, column=0)
-# Creating "Go back to start" Button
-replacedPass_start_btn = Button(
-    fShowReplacedPass, text="Go back to start", command=lambda: raise_frame(fStart))
-replacedPass_start_btn.grid(row=2, column=0)
+    # Notebook
 
-# Select how to Retrieve passwords page
+    my_notebook = ttk.Notebook(fStart)
+    my_notebook.grid(row=0, column=0, pady=5)
 
-# Creating Back Button
-fRetrieve_back_btn = Button(
-    fRetrievePass, text="Back", command=lambda: raise_frame(fStart))
-fRetrieve_back_btn.grid(row=0, column=0)
-# Create Retrieve passwords button
-singleRetrievedPass_btn = Button(
-    fRetrievePass, text="Retrieve password of\na single account", command=lambda: raise_frame(fRetrieveSinglePass))
-singleRetrievedPass_btn.grid(row=1, column=0)
-# Create Save a password button
-allRetrievedPass_btn = Button(fRetrievePass, text="Retrieve all passwords",
-                              command=lambda: raise_frame(fRetrieveAllPass))
-allRetrievedPass_btn.grid(row=1, column=1)
+    # Saving passwords tab
 
-# Retrieving a single password page
+    fSavePass = Frame(my_notebook, width=500, height=700)
+    fSavePass.pack(fill="both", expand=1)
+    my_notebook.add(fSavePass, text="Save Passwords")
 
-# Creating Back Button
-fRetrieveSinglePass_back_btn = Button(
-    fRetrieveSinglePass, text="Back", command=lambda: raise_frame(fRetrievePass))
-fRetrieveSinglePass_back_btn.grid(row=0, column=0)
-# Creating Labels for Retrieving a single
-retrieveAccName_label = Label(fRetrieveSinglePass, text="Enter Account Name: ")
-retrieveAccName_label.grid(row=1, column=0)
-# Creating text boxes for Saved Passwords Page
-retrieveAccName_entry = Entry(fRetrieveSinglePass, width=30)
-retrieveAccName_entry.grid(row=1, column=1)
-retrieveAccName_text = retrieveAccName_entry.get()
-# Creating Enter button
-rEnter_btn = Button(fRetrieveSinglePass, text="Enter")
-rEnter_btn.grid(row=2, column=1)
+    # Retrieving passwords tab
 
-# Retrieving all passwords page
-fRetrieveAllPass_back_btn = Button(
-    fRetrieveAllPass, text="Back", command=lambda: raise_frame(fRetrievePass))
-fRetrieveAllPass_back_btn.grid(row=0, column=0)
-# Showing all passwords
+    fRetrievePass = Frame(my_notebook, width=500, height=500)
+    fRetrievePass.pack(fill="both", expand=1)
+    my_notebook.add(fRetrievePass, text="Retrieve Passwords")
+
+    # Creating labels for save passwords page
+    sAcc_label = Label(fSavePass, text="Username")
+    sAcc_label.grid(row=0, column=0, pady=20, padx=20)
+    sPass_label = Label(fSavePass, text="Password")
+    sPass_label.grid(row=1, column=0, padx=20)
+    # Creating text boxes for save passwords Page
+    sAcc_entry = Entry(fSavePass, width=43)
+    sAcc_entry.grid(row=0, column=1, pady=20, columnspan=2)
+    sPass_entry = Entry(fSavePass, width=20)
+    sPass_entry.grid(row=1, column=1)
+    # Generate Passwords button
+    genPass_btn = Button(
+        fSavePass, text="Generate Password", command=placeholder)
+    genPass_btn.grid(row=1, column=2, padx=5, pady=20, ipadx=10)
+    # Create a Save Password Button to Save the password
+    save_btn = Button(fSavePass, text="Save Password", command=placeholder)
+    save_btn.grid(row=3, column=0, columnspan=3,
+                  pady=20, padx=10, ipadx=100)
+
+    # Creating labels for retrieve passwords page
+    rAcc_label = Label(fRetrievePass, text="Username")
+    rAcc_label.grid(row=0, column=0, pady=20, padx=20)
+    # Creating text boxes for retrieve passwords page
+    rAcc_entry = Entry(fRetrievePass, width=43)
+    rAcc_entry.grid(row=0, column=1, pady=20, columnspan=2)
+    # Create a retrieve a single password button
+    singleRetr_btn = Button(
+        fRetrievePass, text="Retrieve Password", command=placeholder)
+    singleRetr_btn.grid(row=3, column=0, columnspan=3,
+                        pady=20, padx=10, ipadx=100)
+   # Create a retrieve all passwords button
+    allRetr_btn = Button(
+        fRetrievePass, text="Retrieve all passwords", command=placeholder)
+    allRetr_btn.grid(row=4, column=0, columnspan=3, padx=10, ipadx=90)
+    # Create show retrieve pass labels
+    PHText = "Placeholder: 1234jfsoi\ngoogle: google123\nfacebook: 202908hu\nTwitter: ejh0h87re7"
+    rPass_label = Label(fRetrievePass, text="{}".format(PHText))
+    rPass_label.grid(row=5, column=0, columnspan=3, pady=20)
+
+    fStart.mainloop()
 
 
 # Commit changes
