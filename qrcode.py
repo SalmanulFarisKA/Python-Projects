bae39608ad96996bbfbe88288de29278dca0af25
Salmanul Faris <98825604+SalmanulFarisKA@users.noreply.github.com>
2022-05-23 20:02:03 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -2,6 +2,21 @@ import qrcode
 
 data = "Salman"
 
-img = qrcode.make(data)
+qr = qrcode.QRCode(version = 1, box_size = 10, border = 5)
 
-img.save("C:\Users\salma\OneDrive\Documents\Coding\Python Codes\One code everyday\qrcode.png")
+qr.add_data(data)
+
+qr.make(fit = True)
+
+img = qr.make_image(fill_color = 'red', back_color = 'white')
+
+img.save("...\qrcode.png") # idk if this works tbh my vscode stopped working
+
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+img = Image.open("...\qrcode.png")
+
+result = decode(img)
+
+print(result)
