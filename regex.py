a28f5e3d9f72eb296fad3df4f22a37b135c35e9b
Salmanul Faris <98825604+SalmanulFarisKA@users.noreply.github.com>
2022-05-27 00:36:09 +0530

    Add files via upload

---
+++ regex.py
@@ -0,0 +1,15 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get("https://iitpkd.ac.in/people/prithvi")
+
+soup = BeautifulSoup(req.content, "html.parser")
+
+req = soup.body
+
+line = print(req.get_text())  # without the title tag
+
+search = re.match('[\w\.-]+@[\w\.-]+', line)
+
+search.groups()
